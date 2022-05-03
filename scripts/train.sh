eb2e4163d934b54d0afc43ff99a1ef63304662f0
emmavdbold <emma.vdbold@gmail.com>
2022-05-03 13:45:52 +0200

    Add loop over dropout rates in train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -6,21 +6,29 @@ base=$(realpath $scripts/..)
 models=$base/models
 data=$base/data
 tools=$base/tools
+logs=$base/logs
 
 mkdir -p $models
+mkdir -p $logs
 
 num_threads=4
 device=""
 
 SECONDS=0
 
-(cd $tools/pytorch-examples/word_language_model &&
+dropout_rates=(0 0.3 0.4 0.5 0.8)
+
+cd $tools/pytorch-examples/word_language_model
+
+for d in ${dropout_rates[*]}
+do
     CUDA_VISIBLE_DEVICES=$device OMP_NUM_THREADS=$num_threads python main.py --data $data/grimm \
         --epochs 40 \
         --log-interval 100 \
-        --emsize 200 --nhid 200 --dropout 0.5 --tied \
-        --save $models/model.pt
-)
+        --emsize 200 --nhid 200 --dropout $d --tied \
+        --seed 1111 \
+        --save $models/model.$d.pt > $logs/model.$d.log
+done
 
 echo "time taken:"
 echo "$SECONDS seconds"
